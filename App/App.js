002e48c37c4171a6794439cb591c3237eb4ed037
nathb <nathb2b@gmail.com>
2019-06-26 12:17:03 +0200

    + evt cat swiper

--- App/App.js
+++ App/App.js
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import "core-js";
+import "core-js/features/object/from-entries";
 import "regenerator-runtime/runtime";
 import React                                      from 'react';
 import moment                                     from 'moment';
