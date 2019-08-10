056d0d5ba77452233a92ace9264360cd9b8b0cb0
nathb <nathb2b@gmail.com>
2019-08-10 17:33:17 +0200

    ^

--- App/ui/components/SearchBar.js
+++ App/ui/components/SearchBar.js
@@ -21,6 +21,7 @@ import moment                    from "moment";
 import React                     from "react";
 import {scopeToProps, withScope} from "react-scopes";
 import {asStore}                 from "rescope-spells";
+import {Comps}                   from "../index";
 
 @withScope(
 	{
@@ -85,6 +86,8 @@ export default class SearchBar extends React.Component {
 						//||
 						(moment(day).format("dddd DD MMMM YYYY"))
 					}
+					
+					<Comps.LoginBox/>
 				</div>
 				<TextField
 					className={"input"}
